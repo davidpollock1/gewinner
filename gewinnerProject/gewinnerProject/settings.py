5c7c9da85d66e4c5bee714cd98a0ad32cc22d5ac
David <david@Davids-MacBook-Pro-2.local>
2024-09-09 17:54:57 -0600

    remove key.

--- gewinnerProject/gewinnerProject/settings.py
+++ gewinnerProject/gewinnerProject/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1o2ff1qin%t_-$cqc31kl(b++8(#%_w)2&+8m&v2ue&5ofx7_y'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
